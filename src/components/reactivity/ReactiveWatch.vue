73c8ac2679e45b043a65bc831b441e8ea76a8240
Christian <c.espinoza.code@gmail.com>
2023-11-01 11:47:09 -0500

    reactive watch example added

---
+++ src/components/reactivity/ReactiveWatch.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { reactive, watch } from 'vue';
+
+// Reactive works with objects
+const stateReactive = reactive({
+  text: 'Testing',
+  counter: 0,
+});
+
+setInterval(() => {
+  stateReactive.counter++;
+}, 500);
+
+watch(() => stateReactive.counter, (newValue, oldValue) => {
+  console.log(newValue, oldValue);
+})
+</script>
+
+<template>
+  <div>
+    <p>{{ stateReactive }}</p>
+  </div>
+</template>
+
+<style scoped></style>
