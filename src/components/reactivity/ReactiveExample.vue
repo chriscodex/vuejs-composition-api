fffd9c83c727ea60c31d78680183312f954a1603
Christian <c.espinoza.code@gmail.com>
2023-11-01 11:40:00 -0500

    reactive example created

---
+++ src/components/reactivity/ReactiveExample.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+// Reactive works with objects
+const stateReactive = reactive({
+  text: 'Testing',
+  counter: 0,
+});
+
+setInterval(() => {
+  stateReactive.counter++;
+}, 500);
+</script>
+
+<template>
+  <div>
+    <p>{{ stateReactive }}</p>
+  </div>
+</template>
+
+<style scoped></style>
