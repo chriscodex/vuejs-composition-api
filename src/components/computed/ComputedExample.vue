1ca6998fe0faa6b45696a985b589de7156b81cae
Christian <c.espinoza.code@gmail.com>
2023-11-01 12:13:38 -0500

    computed example added

---
+++ src/components/computed/ComputedExample.vue
@@ -0,0 +1,22 @@
+<script setup lang='ts'>
+import { ref, computed } from 'vue';
+
+const firstName = ref('Christian')
+const lastName = ref('Espinoza')
+
+const fullName = computed(() => {
+  return `${firstName.value} ${lastName.value}`
+})
+
+</script>
+
+<template>
+  <div>
+    <h3>Computed</h3>
+    <p>Fullname: {{ fullName }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
