e3e97f7a3ae73f03b962a3b1afaf302ad241baec
Christian <c.espinoza.code@gmail.com>
2023-11-01 14:59:59 -0500

    Child for provideInject example created

---
+++ src/components/provideInject/ChildC.vue
@@ -0,0 +1,15 @@
+<script setup lang='ts'>
+import { inject } from 'vue';
+
+const username = inject('username');
+</script>
+
+<template>
+  <div>
+    <p>Username: {{ username }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
