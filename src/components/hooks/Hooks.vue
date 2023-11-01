5f9c1cd03ceda34d52bbeb30a0885bd592337cf2
Christian <c.espinoza.code@gmail.com>
2023-11-01 11:33:05 -0500

    hooks example created

---
+++ src/components/hooks/Hooks.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onBeforeMount, onMounted } from 'vue';
+
+const text = 'Testing Hooks';
+
+onBeforeMount(() => {
+  console.log('onBeforeMount');
+})
+
+onMounted(() => {
+  console.log('onMounted');
+})
+</script>
+
+<template>
+  <div>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<style scoped></style>
