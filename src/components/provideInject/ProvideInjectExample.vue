6d46c74fc6d14fc0cf5e31ef265aa40b0f41e959
Christian <c.espinoza.code@gmail.com>
2023-11-01 15:00:13 -0500

    parent for provideInject example created

---
+++ src/components/provideInject/ProvideInjectExample.vue
@@ -0,0 +1,18 @@
+<script setup lang='ts'>
+import { provide } from 'vue';
+import ChildC from './ChildC.vue';
+
+provide('username', 'Christian')
+
+</script>
+
+<template>
+  <div>
+    <h3>Provide Inject</h3>
+    <ChildC />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
