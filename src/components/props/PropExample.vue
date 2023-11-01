a395363b76e3f18820f250857ffe0e933bc21c8f
Christian <c.espinoza.code@gmail.com>
2023-11-01 14:41:43 -0500

    Prop exame with toRefs for reactivity agregada

---
+++ src/components/props/PropExample.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed, toRefs } from 'vue';
+
+const { firstName, lastName } = toRefs(
+  defineProps({
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+  })
+);
+
+const fullName = computed(() => {
+  return `${firstName} ${lastName}`;
+});
+</script>
+
+<template>
+  <div>
+    <h3>Props</h3>
+    <p>Fullname: {{ fullName }}</p>
+  </div>
+</template>
+
+<style scoped></style>
