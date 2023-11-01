a1e3a6135d052d3350fb331c71a4d4fd5a6d64ea
Christian <c.espinoza.code@gmail.com>
2023-11-01 11:34:21 -0500

    dinamic component example created

---
+++ src/components/dinamicComponent/DinamicComponent-1.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HelloWorld from './HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+  <!-- <component :is="HelloWorld" msg="Testing"></component> -->
+</template>
