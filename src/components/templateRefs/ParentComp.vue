a351dbff077a06d9af5247e125a55a6c9c21420e
Christian <c.espinoza.code@gmail.com>
2023-11-01 15:30:47 -0500

    example for templateRef created

---
+++ src/components/templateRefs/ParentComp.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const btn = ref(null);
+
+watch(btn, () => {
+  console.log(btn.value);
+});
+</script>
+
+<template>
+  <div>
+    <h3>Template Ref</h3>
+    <button ref="btn">Click</button>
+  </div>
+</template>
+
+<style scoped></style>
