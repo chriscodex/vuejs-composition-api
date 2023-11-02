6a82f0b2928055d45f7cc4cf4bcada68efcf2095
Christian <c.espinoza.code@gmail.com>
2023-11-02 15:38:51 -0500

    script setup added to app.vue

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@ import ReactiveWatch from './components/reactivity/ReactiveWatch.vue';
 import ComputedExample from './components/computed/ComputedExample.vue';
 import PropExample from './components/props/PropExample.vue';
 import ProvideInjectExample from './components/provideInject/ProvideInjectExample.vue';
+import ParentComp from './components/templateRefs/ParentComp.vue';
+import ScriptSetup from './components/scripSetup/ScriptSetupExample.vue';
 </script>
 
 <template>
@@ -20,4 +22,6 @@ import ProvideInjectExample from './components/provideInject/ProvideInjectExampl
   <ComputedExample />
   <PropExample first-name="Christian" last-name="Espinoza" />
   <ProvideInjectExample />
+  <ParentComp />
+  <ScriptSetup first-name="Christian"/>
 </template>
