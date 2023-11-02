93c1b3f881a8372952b02f635274aa865e83971c
Christian <c.espinoza.code@gmail.com>
2023-11-02 15:38:18 -0500

    script setup example created

---
+++ src/components/scripSetup/ScriptSetupExample.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineProps, toRefs, defineExpose } from 'vue';
+const props = defineProps({
+  firstName: {
+    type: String,
+  },
+});
+
+const { firstName } = toRefs(props);
+
+defineExpose({
+  firstName
+})
+</script>
+
+<template>
+  <h3>Script Setup</h3>
+  <p>{{ firstName }}</p>
+</template>
+
+<style scoped></style>
